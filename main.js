597f14f1b9b5c73411256c5c67f8ef0cba491a3b
VEROFOREST <xvforest@orange.fr>
2020-11-24 16:10:59 +0100

    first push

---
+++ main.js
@@ -0,0 +1,33 @@
+
+// anime les liens de la barre de navigation du footer 
+$(function(){
+
+    $(".navbar a, footer #top").on("click", function(event){
+    
+        event.preventDefault();
+        var hash = this.hash;
+        
+        $('body,html').animate({scrollTop: $(hash).offset().top} , 2000 , function(){window.location.hash = hash;})
+        
+    });
+
+})
+ $(function(){
+    $('#phone').click(function(){
+       $('#number-phone').toggle() // AFFICHE ET CACHE A CHAQUE CLIQUE SUR LE BOUTTON MON NUMERO
+    });
+ });
+
+document.querySelector(".card-flip").classList.toggle("flip");
+
+/* 
+ * Holder.js for demo image
+ * Just for demo purpose
+ */
+// Holder.addTheme('gray', {
+//   bg: '#777',
+//   fg: 'rgba(255,255,255,.75)',
+//   font: 'Helvetica',
+//   fontweight: 'normal'
+
+// }
